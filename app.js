0e59cc39aaf1b24398f8cf5f3a54f768d0c4882f
Marko <marko.uremovic00@gmail.com>
2023-11-30 19:19:30 +0100

    added cors

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use('/api', apiRoutes);
+app.use(cors());
+app.use("/api", apiRoutes);
 
-app.get('', (_, res) => {
-	res.send('Welcome to the SUT agent App.');
+app.get("", (_, res) => {
+  res.send("Welcome to the SUT agent App.");
 });
 
 app.listen(port, () => {
-	console.log(`Agent app listening on port ${port}`);
+  console.log(`Agent app listening on port ${port}`);
 });
