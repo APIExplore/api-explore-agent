c7a85f4b915fb8e6942da0c95cdd29579377907b
Felix <fb52683@fer.hr>
2024-01-06 18:55:54 +0100

    feat: #11 Add customizable java path
    Enhanced parsing of startCommand.
    
    Added an optional javaPath variable for overriding default java executor when starting an API.
    
    ref: https://github.com/APIExplore/api-explore-agent/issues/11

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,18 +7,36 @@ const path = require('path');
 
 const apiInfo = require('../API-info.json');
 
-async function startSut(responseMsg, res, sutApi) {
+async function startSut(responseMsg, res, sutApi, javaPath) {
 	const jarFilePath = path.join(__dirname, '../compiled', sutApi.jarFileName);
-	const command = sutApi.startCommand.replace('{{FILE_PATH}}', jarFilePath);
-	const splitted_command = command.split(' ');
+
+	let command;
+	if (javaPath) {
+		command = `"${javaPath}" -jar "${jarFilePath}"`;
+	} else {
+		command = sutApi.startCommand.replace('{{FILE_PATH}}', jarFilePath);
+	}
+
+	const regex = /[^\s"]+|"([^"]*)"/gi;
+	const splitted_command = [];
+
+	let match;
+	do {
+		match = regex.exec(command);
+		if (match != null) {
+			splitted_command.push(match[1] ? match[1] : match[0]);
+		}
+	} while (match != null);
 
 	const child = spawn(splitted_command[0], splitted_command.slice(1));
 	console.log('Child process id', child.pid);
 
+	let apiStarted = false;
 	child.stdout.on('data', (data) => {
 		console.log(`stdout: ${data}`);
 
 		if (data.toString().includes(sutApi.onStartPhrase)) {
+			apiStarted = true;
 			res.send({
 				message: responseMsg,
 				PID: `${child.pid}`,
@@ -30,11 +48,19 @@ async function startSut(responseMsg, res, sutApi) {
 		console.log(`stderr: ${data}`);
 	});
 
-	child.on('error', (error) => console.log(`error: ${error.message}`));
+	child.on('error', (error) => {
+		console.log(`error: ${error.message}`);
+		if (!apiStarted) {
+			res.status(500).send('API failed to start properly');
+		}
+	});
 
 	child.on('exit', (code, signal) => {
 		if (code) console.log(`Process exit with code: ${code}`);
 		if (signal) console.log(`Process killed with signal: ${signal}`);
+		if (!apiStarted) {
+			res.status(500).send('API failed to start properly');
+		}
 		console.log(`Done ✅`);
 	});
 }
@@ -47,13 +73,14 @@ router.get('/', (_, res) => {
 
 router.get('/start-api/:id', async function (req, res) {
 	const apiId = parseInt(req.params.id, 10);
+	const javaPath = req.query.javaPath;
 	const sutApi = apiInfo.apiList.find((api) => api.id === apiId);
 
 	if (!sutApi) {
 		return res.status(404).send('API not found');
 	}
 
-	return await startSut('API Started Successfully!!!', res, sutApi);
+	return await startSut('API Started Successfully!!!', res, sutApi, javaPath);
 });
 
 router.post('/stop-api', (req, res) => {
@@ -76,7 +103,7 @@ router.post('/stop-api', (req, res) => {
 });
 
 router.post('/restart-api', (req, res) => {
-	const { id, pid } = req.body;
+	const { id, pid, javaPath } = req.body;
 	const sutApi = apiInfo.apiList.find((api) => api.id === parseInt(id, 10));
 	if (!sutApi) {
 		return res.status(404).send('API not found');
@@ -92,7 +119,7 @@ router.post('/restart-api', (req, res) => {
 			console.error(`Error executing command: ${error.message}`);
 			return res.status(500).send('Internal Server Error');
 		}
-		return await startSut('API Restarted Successfully!!!', res, sutApi);
+		return await startSut('API Restarted Successfully!!!', res, sutApi, javaPath);
 	});
 });
 
